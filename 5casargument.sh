ebdda1701b1b22784fb3e98d05e9e31ebf3f830a
Rayko0 <166178255+Rayko0@users.noreply.github.com>
2024-12-11 14:06:42 +0100

    Update 5casargument.sh

--- 5casargument.sh
+++ 5casargument.sh
@@ -49,32 +49,3 @@ case "$combined" in
 "lv-allminmax") lvallminmax;;
 *) echo "rtfm";;
 esac
-> t.csv
-	awk -F';' -v a=2 '{ if($a !~ /-/ && $(($a+1)) !~ /-/) print $a, $(NF-1), $NF }' t.csv
-}
-
-function lvall {
-	echo "tout roule"
-}
-	
-function lvcomp {
-	echo "tout roule"
-}
-
-function lvindiv {
-	echo "tout roule"
-}
-
-combined="$2-$3"
-fichier="$1"
-
-
-
-case "$combined" in
-"hvb-comp") hvbcomp;;
-"hva-comp") hvacomp;;
-"lv-all") lvall;;
-"lv-comp") lvcomp;;
-"lv-indiv") lvindiv;;
-*) echo "rtfm";;
-esac
