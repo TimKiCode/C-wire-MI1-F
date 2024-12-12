01b2372f9e428e9184c52cb6f4fb33ef39ead866
Rayko0 <166178255+Rayko0@users.noreply.github.com>
2024-12-12 09:09:10 +0100

    shell.sh

--- 5casargument.sh
+++ 5casargument.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Nom du dossier à vérifier (chemin relatif)
+tmp="tmp"
+graphs="graphs"
+exec="exec.c"
+combined="$2-$3"
+fichier="$1"
+
+touch tmp.csv
+
 function hvbcomp {
 	echo "tout roule"
 	tail -n +2 "$fichier"  | cut -d ';' -f 2-> t.csv
@@ -39,10 +48,34 @@ function lvallminmax {
 	awk -F';' '{ if($3 !~ /-/) print $3, $(NF-1), $NF }' t.csv > tmp.csv
 }
 
-combined="$2-$3"
-fichier="$1"
+# Vérification
+if [ -d "$tmp" ]; then
+	echo "Le dossier '$tmp' existe. Je verifie qu'il est vide."
+	if [ -n "$(ls -A "$tmp")" ]; then
+		echo "Le dossier '$tmp' n'est pas vide. Je supprime son contenu."
+		rm -rf "$tmp"/*
+	else
+		echo "Le dossier '$tmp' est déjà vide."
+	fi
+else
+	echo "Le dossier '$tmp' n'existe pas. Donc je vais le créer."
+	mkdir "$tmp"
+fi
 
-touch tmp.csv
+if [ -d "$graphs" ]; then
+	echo "Le dossier '$graphs' existe."
+else
+	echo "Le dossier '$graphs' n'existe pas. Donc je vais le créer."
+	mkdir "$graphs"
+    
+fi
+
+if [ -f "$exec" ]; then
+	echo "Le dossier '$exec' existe."
+else
+	echo "Erreur sur l'excutable .c '$exec' n'existe pas."
+    
+fi
 
 case "$combined" in
 "hvb-comp") hvbcomp;;
